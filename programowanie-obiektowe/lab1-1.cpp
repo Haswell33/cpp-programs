80b60822367f61999fc1a5fcf40571c9b4dba9f2
Haswell33 <31355807+Haswell33@users.noreply.github.com>
2021-11-09 22:35:39 +0100

    Update lab1-1.cpp

--- programowanie-obiektowe/lab1-1.cpp
+++ programowanie-obiektowe/lab1-1.cpp
@@ -1,3 +1,17 @@
+/*
+- zad1 - napisz funkcje wczytaj(); ktora otrzymuje referencje do obiektu i  wprowadza dane do klasy, a nastepnie napisz funkcje wypisz(), ktora otrzymuje obiekt i wypisuje jego pola/atrybuty na ekran
+- zad2 - dopisz(zamien z funkcji) metode wczytujaca i wypisujaca 
+- zad3 - napisz klase student ktora bedzie sluszyla do przechowywania informacji o studentach, imie naziwsko numer studenta, powinna udostepniac metody publiczne do wczytywania danych i wypisywania na ekran,. napisz klase zeby dostep do zawartych w niej pol byl dostep do niej tylko za posrednictwem tej klasy
+- zad4 - do klasy Student dodaj pole wskaznikowe na double wskazujace na tablice sluzaca do prowadzenia ewidencji ocen oraz dodaj pole statyczne ilosc_studentow i dopisz nastepujace metody publiczne:</br>
+	- inicjuj - ktora ustawia ilosc ocen na 0 oraz rezerwuje pamiec na 10elementowa tablice o elementach typu double (oceny) i zapisuje w polu wsk wskaznik do nowo utworzonej tablicy;
+	- zwolnij - zwalniajaca obszar pamieci wskazywanya przez pole wsk i nadajaca temu polu wartosc NULL
+	- wpisz_ocena - wpisujaca koleje ocene studenta
+	- sr_ocen_studenta - obliczajaca i wsypisujaca srednia z ocen studenta na standardowe wyjscie
+- zad5 - do klasy student dopisz:
+	- bezargumentowy konstruktor, zastêpuj¹cy metode inicjuj()
+	- dekonstruktor zastepujacy
+- zad6 - napisz program ktory pozwoli na ewidencje grupy 20 studentow z wykorzystaniem klasy student z zadania 1.7 uwgaa nr studenta powiniene byc nadawany podczas incicjacji
+*/
 #include <iostream>
 
 using namespace std;
