36172847d860214271661101fad69a313e8f9f61
Haswell33 <31355807+Haswell33@users.noreply.github.com>
2021-11-09 21:48:22 +0100

    Create sito-erastotenesa.cpp

---
+++ sito-erastotenesa.cpp
@@ -0,0 +1,91 @@
+#include <iostream>
+#include <fstream>
+#include <cstdlib>
+
+using namespace std;
+
+typedef struct lista_pierwsze
+{
+	int data;
+	struct lista_pierwsze *next;	
+}l_p;
+
+l_p* create_list(int n)
+{
+	l_p *nowy, *head = NULL;
+	for(int i=n; i>1; i--)
+	{
+		nowy = (l_p*)malloc(sizeof(l_p));
+		nowy->data = i;
+		nowy->next = head;
+		head = nowy;
+	}
+	return head;
+}
+
+void sito_eratostensa(l_p *head)
+{
+	l_p *x = head, *y, *z;
+	int i;
+	while(x)
+	{
+		
+		i = x->data;
+		//cout << data%i << endl;
+		y = x;
+		z = x->next;
+		
+		while(z)
+		{
+			if(!(z -> data%i))
+			{
+				y->next = z->next;
+				free(z);
+				z = y->next;	
+			}
+			else
+			{
+				y = z;
+				z = y->next;
+			}
+		}	
+		x = x->next;	
+	}	
+}
+
+void save_to_file(l_p* head, int n)
+{
+	fstream file;
+	file.open("pierwsze.txt", ios::out);
+	file << "Liczby pierwsze z zakresu (2..." << n << "): " << endl;
+	if(head==NULL)
+	{
+		return;
+	}
+	while(head)
+	{
+		file << head->data << endl;
+		head = head->next;
+	}	
+	if(file.good() == true)
+	{
+		file.close();
+	}	
+}
+
+int main()
+{
+	int n=0;
+	l_p *head=NULL;
+	cout << "Okres gorny zbioru: ";
+	while(!(cin >> n)) 
+	{
+	    cout << "Dana liczba musi byc w systemie dziesietnym oraz wieksza od 1: ";
+	    cin.clear(); 
+	    cin.sync(); 
+	}
+	head = create_list(n);
+	sito_eratostensa(head);
+	save_to_file(head, n);
+	return 0;
+}
